c248f5feb1bfccfc430a20f941e856f988a6a531
somin2352 <somin030502@gmail.com>
2025-02-05 18:40:51 +0900

    수량 버튼 증감버튼 기능 구현
    - useState로 count 상태 관리

--- src/components/cart-count-button/count-button.tsx
+++ src/components/cart-count-button/count-button.tsx
@@ -1,13 +1,28 @@
+import { useState } from 'react';
 import './count-button.css';
 
 function CountButton() {
+  const [count, setCount] = useState<number>(0);
+
+  const handleCountMinus = () => {
+    if (count === 0) return;
+
+    const nextCount = count - 1;
+    setCount(nextCount);
+  };
+
+  const handleCountPlus = () => {
+    const nextCount = count + 1;
+    setCount(nextCount);
+  };
+
   return (
     <div className="countButton">
-      <button type="button" className="minus">
+      <button type="button" className="minus" onClick={handleCountMinus}>
         -
       </button>
-      <span className="countNumber">0</span>
-      <button type="button" className="plus">
+      <span className="countNumber">{count}</span>
+      <button type="button" className="plus" onClick={handleCountPlus}>
         +
       </button>
     </div>
