023b11248985eaa6bba9c0194f7fb4d82de601a9
somin2352 <somin030502@gmail.com>
2025-02-16 14:43:04 +0900

    query값과 일치하는 데이터 필러팅하여 렌더링

--- src/components/searched-list/searched-list.tsx
+++ src/components/searched-list/searched-list.tsx
@@ -4,14 +4,30 @@ import './searched-list.css';
 
 interface SearchedListProps {
   list: UserInfo[];
+  query: string;
 }
 
-function SearchedList({ list }: SearchedListProps) {
+function SearchedList({ list, query }: SearchedListProps) {
+  const words = query
+    .split(' ')
+    .filter(Boolean)
+    .map((word) => word.toLowerCase().trim());
+
+  const filteredList = list.filter((item) =>
+    words.every(
+      (word) =>
+        item.name.toLowerCase().includes(word) || item.tags.includes(word)
+    )
+  );
+
+  const isEmpty = filteredList.length === 0;
+
   return (
     <div className="searched-list">
-      {list.map((item) => (
-        <Card item={item} key={item.id} />
-      ))}
+      {isEmpty && <p>&ldquo;{query}&ldquo; 검색된 정보가 없습니다..🥲</p>}
+
+      {!isEmpty &&
+        filteredList.map((item) => <Card key={item.id} item={item} />)}
     </div>
   );
 }
