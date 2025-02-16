dc3f507663669bc4a7ff9cabea61955277297a84
somin2352 <somin030502@gmail.com>
2025-02-16 14:40:05 +0900

    하위 컴포넌트에 사용자 데이터 전달 & query 정보 전달

--- src/pages/card-search-list/card-search-list.tsx
+++ src/pages/card-search-list/card-search-list.tsx
@@ -3,16 +3,32 @@ import SearchedList from '@/components/searched-list/searched-list';
 import './card-search-list.css';
 import users from '@/data/card/card';
 import { type UserInfo } from '@/data/card/type';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { getQueryParam } from '@/utils/search-params';
+
+const getQueryState = () => getQueryParam() ?? '';
 
 function CardSearchList() {
   const [userList] = useState<UserInfo[]>(users);
+  const [query, setQuery] = useState(getQueryState);
+
+  useEffect(() => {
+    const handlePopState = () => {
+      setQuery(getQueryState);
+    };
+
+    globalThis.addEventListener('popstate', handlePopState);
+
+    return () => {
+      globalThis.removeEventListener('popstate', handlePopState);
+    };
+  }, []);
 
   return (
     <div className="card-search-list">
-      <SearchForm />
+      <SearchForm query={query} setQuery={setQuery} />
       <div className="card-list">
-        <SearchedList list={userList} />
+        <SearchedList list={userList} query={query} />
       </div>
     </div>
   );
