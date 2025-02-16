9c4fee24f0524ae818a69f7fd3e92fbc41887526
somin2352 <somin030502@gmail.com>
2025-02-16 12:42:57 +0900

    유저 정보 데이터

---
+++ src/data/card/card.ts
@@ -0,0 +1,40 @@
+const users = [
+  {
+    id: 1,
+    name: 'Lelah Nichols',
+    image: '/public/user-img/user1.jpg',
+    tags: ['clothes', 'stem'],
+  },
+  {
+    id: 2,
+    name: 'Jesus Wiess',
+    image: '/public/user-img/user2.jpg',
+    tags: ['headset', 'gadget', 'speed', 'winter'],
+  },
+  {
+    id: 3,
+    name: 'Annie Rice',
+    image: '/public/user-img/user3.jpg',
+    tags: ['road', 'mountain', 'trip', 'earth', 'nature'],
+  },
+  {
+    id: 4,
+    name: 'Robert Brower',
+    image: '/public/user-img/user4.jpg',
+    tags: ['Maintenance', 'gears', 'frames', 'repair'],
+  },
+  {
+    id: 5,
+    name: 'Amy Campbell',
+    image: '/public/user-img/user5.jpg',
+    tags: ['music', 'disks'],
+  },
+  {
+    id: 6,
+    name: 'Anthony S. Morin',
+    image: '/public/user-img/user6.jpg',
+    tags: ['vintage', 'electric'],
+  },
+];
+
+export default users;
---
+++ src/data/card/type.ts
@@ -0,0 +1,4 @@
+import users from './card';
+
+export type Users = typeof users;
+export type UserInfo = Users[number];
